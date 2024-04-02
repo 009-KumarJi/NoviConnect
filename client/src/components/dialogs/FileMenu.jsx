213efee80ed3c9f931df2acb4daee339b67668b4
009-KumarJi <krishnakumar091@yahoo.com>
2024-04-02 05:48:05 +0530

    FileMenu of Designed

---
+++ client/src/components/dialogs/FileMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Menu} from "@mui/material";
+
+const FileMenu = ({anchorE1}) => {
+  return (
+    <Menu open={false} anchorEl={anchorE1}>
+      <div
+        style={{
+          width: '10rem',
+        }}
+      >
+        quaestio sanctus ex per nulla quod cras dissentiunt ad ocurreret volumus suscipiantur ceteros laoreet ut recteque inciderint repudiandae idque euismod
+      </div>
+    </Menu>
+  );
+};
+
+export default FileMenu;
