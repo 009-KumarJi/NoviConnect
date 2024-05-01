551eb3bb4d3f90737e7326253d80bb64a68ba85e
009-KumarJi <krishnakumar091@yahoo.com>
2024-05-01 19:19:38 +0530

    server socket created for message relay

---
+++ server/lib/socketio.helper.js
@@ -0,0 +1,14 @@
+import {activeUserSocketIds} from "../server.js";
+
+export const getSockets = (users) => users.map(user => activeUserSocketIds.get(user._id.toString())); // sockets array
+
+export const createRoom = async (sockets, roomId, io) => {
+  const room = `Room_${roomId}`;
+  for (const socketId of sockets) {
+    const socket = io.sockets.sockets.get(socketId);
+    if (socket) {
+      await new Promise((resolve) => socket.join(room, resolve));
+    }
+  }
+  return room;
+}
--- server/server.js
+++ server/server.js
@@ -8,10 +8,20 @@ import {errorMiddleware} from "./middlewares/error.middleware.js";
 import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
+import {Server} from "socket.io";
+import {createServer} from "http";
+import {NEW_MESSAGE, NEW_MESSAGE_ALERT} from "./constants/events.constant.js";
+import {v4 as uuid} from "uuid";
+import {getSockets} from "./lib/socketio.helper.js";
+import {Message} from "./models/message.model.js";
 
 dotenv.config({path: "./.env"});
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {});
+const activeUserSocketIds = new Map();
+
 const port = process.env.PORT || 3000;
 const envMode = process.env.NODE_ENV || "PRODUCTION";
 const adminKey = process.env.ADMIN_SECRET_KEY;
@@ -30,14 +40,66 @@ app.use("/krishna-den", adminRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+const user = {
+  _id: "1234567890",
+  name: "Krishna"
+};
+io.on("connection", (socket) => {
+  console.log(`socketId: ${socket.id} connected!`);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} in ${envMode} mode.`);
+  socket.on(NEW_MESSAGE, async ({ChatId, members, message}) => {
+
+
+    activeUserSocketIds.set(user._id.toString(), socket.id);
+
+    const messageForRealTime = {
+      content: message,
+      _id: uuid(),
+      sender: {
+        _id: user._id,
+        name: user.name
+      },
+      chat: ChatId,
+      createdAt: new Date().toString()
+    }
+    const messageForDB = {
+      content: message,
+      sender: user._id,
+      chat: ChatId
+    }
+    const membersSockets = getSockets(members);
+    io.to(membersSockets).emit(NEW_MESSAGE, {ChatId, message: messageForRealTime, mem: membersSockets});
+    io.to(membersSockets).emit(NEW_MESSAGE_ALERT, {ChatId});
+    // TODO: Implement Room Building then send data to that room
+    /*const roomId = await createRoom(membersSockets, ChatId, io);
+    io.to(roomId).emit(NEW_MESSAGE, {
+      message: messageForRealTime,
+      chat: ChatId
+    });
+    console.log(`Message sent to room: ${roomId}`);
+    io.to(roomId).emit(NEW_MESSAGE_ALERT, {ChatId})*/
+
+    try{
+      await Message.create(messageForDB);
+    } catch (e) {
+      console.log(e);
+    }
+  })
+
+  socket.on("disconnect", () => {
+    activeUserSocketIds.delete(user._id);
+    console.log(`socketId: ${socket.id} disconnected!`);
+  });
 });
 
 app.use(errorMiddleware);
 
-export {envMode, adminKey};
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${envMode} mode.`);
+});
+
+
+export {envMode, adminKey, activeUserSocketIds};
 
 // Path: server/server.js
 
